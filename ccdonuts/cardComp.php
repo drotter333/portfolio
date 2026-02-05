78ecbb53025b7540f93ab68ce303a7a744129f84
drotter333 <drotter0921@gmail.com>
2026-02-05 15:25:55 +0900

    Add files via upload

--- ccdonuts/cardComp.php
+++ ccdonuts/cardComp.php
@@ -7,8 +7,7 @@ $sql=$pdo->prepare('insert into card values(null,?,?,?,?,?,?)');
 $sql->execute([
     $_SESSION['customers']['id'], $_REQUEST['card_number'], $_REQUEST['card_brand'],  
     $_REQUEST['month'], $_REQUEST['year'], $_REQUEST['security_code']]);
-$_SESSION['card'] = [
-    'card_brand' => $_REQUEST['card_brand']];
+$_SESSION['card'] = ['card_brand' => $_REQUEST['card_brand']];
 echo '<div class="mainTitle"><h1>カード情報登録完了</h1></div>';
 echo '<div class="compWrapp">';
 echo '<p>支払い情報登録が完了しました。</p>';
